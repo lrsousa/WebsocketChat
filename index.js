f5e35f2227661e802785b5681cf4fedca50e3adc
rsleonardo <leonarrangel@gmail.com>
2016-06-19 18:06:10 -0300

    alteracoes de layout, add typing event, nickname, nickname colors, logoutevent, usersnumber count

--- index.js
+++ index.js
@@ -6,20 +6,72 @@ app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket) {
-	socket.broadcast.emit('olá');
-	console.log('alguem se conectou');
-	socket.on('disconnect', function(){
-    	console.log('user disconnected');
-	});
-	socket.on('chat message', function(msg){
-	    console.log('message: ' + msg);
-	});
-	socket.on('chat message', function(msg){
-		io.emit('chat message', msg);
-	});
+app.get('/resources/style.css', function(req, res) {
+	res.sendFile(__dirname + '/resources/style.css');
 });
 
-http.listen(8080, function() {
-	console.log("listening on *:8080");
+app.get('/resources/main.js', function(req, res) {
+	res.sendFile(__dirname + '/resources/main.js');
+});
+
+var port = 8080;
+http.listen(port, function() {
+	console.log("listening on *:" + port);
+});
+
+var numUsers = 0;
+
+io.on('connection', function(socket) {
+	var addedUser = false;
+
+	//quando o client envia uma 'new message' esse listen executa
+	socket.on('new message', function(msg){
+		socket.broadcast.emit('new message', {
+			username : socket.username,
+			message : msg
+		});
+	});
+
+	socket.on('add user', function(username) {
+		if(addedUser) return;
+		//registrando nome do usuario
+		socket.username = username;
+		++numUsers;
+		addedUser = true;
+		socket.emit('login', {
+			numUsers: numUsers
+		});
+
+		socket.broadcast.emit('user joined', {
+			username : socket.username,
+			numUsers : numUsers
+		});
+	});
+
+	//quando client emite 'typing', isso é compartilhado com os outros
+	socket.on('typing', function() {
+		socket.broadcast.emit('typing', {
+			username : socket.username
+		});
+	});
+
+	//quando client emite 'stop typing' isso é compartilhado
+	socket.on('stop typing', function() {
+		socket.broadcast.emit('stop typing', {
+			username : socket.username
+		});
+	});
+
+	//quando user disconecta...
+	socket.on('disconnect', function() {
+		if(addedUser) {
+			--numUsers;
+
+			//avisa a todos que o client saiu
+			socket.broadcast.emit('user left', {
+				username : socket.username,
+				numUsers : numUsers
+			});
+		}
+	});
 });
